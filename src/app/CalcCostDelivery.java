7a731f0b125afd1b454d48e790c3bd8de9a8be9f
Oleksiy Bechke <bechke@dlit.dp.ua>
2024-06-21 16:45:11 +0300

    Initial commit

---
+++ src/app/CalcCostDelivery.java
@@ -0,0 +1,16 @@
+package app;
+
+// Клас розрахунку вартості товару,
+// враховуючи вартість доставки
+public class CalcCostDelivery extends CalcCostBase {
+
+    // Вартість доставки
+    private final static double deliveryPrice = 7;
+
+    // Розрахунок вартості товару,
+    // з урахуванням вартості доставки
+    @Override
+    public double calcCost(Product product) {
+        return super.calcCost(product) + deliveryPrice;
+    }
+}
